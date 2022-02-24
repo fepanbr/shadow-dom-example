05ae028d7f9c341c8ed1646dd25c784a7b8a11d9
fepanbr <fepanbr@gmail.com>
2022-02-24 22:34:44 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import logo from './logo.svg';
-import './App.css';
+import root from 'react-shadow';
+
+const styles = `
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+p {
+  color: yellow;
+}
+
+`;
 
 function App() {
   return (
+    <root.div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +66,8 @@ function App() {
         </a>
       </header>
     </div>
+    <style type="text/css">{styles}</style>
+    </root.div>
   );
 }
 
